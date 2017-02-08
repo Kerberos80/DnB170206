38afe12bc38bf550cfa7b9ba593c3c83eb0dd261
Kerberos80 <80rleo99!!>
2017-02-08 17:38:47 +0900

    회원 DAO 추가

---
+++ DVDnBluray/src/memeber/dao/MemberDAO.java
@@ -0,0 +1,68 @@
+package memeber.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import jdbc.JdbcUtil;
+import member.model.Member;
+
+// 회원 관련 DAO
+public class MemberDAO {
+	
+	String query;	
+	
+	// 회원 id 로 검색 : 회원가입 시 ID 충복 체크, 회원정보수정 때 사용 
+	public Member selectById(Connection connection, String id) throws SQLException {
+		
+		PreparedStatement preparedStatement = null;
+		ResultSet resultSet = null;
+		
+		try {
+			
+			query = "select * from member where id = ?";
+			preparedStatement = connection.prepareStatement(query);
+			preparedStatement.setString(1, id);
+			resultSet = preparedStatement.executeQuery();
+			
+			Member member = null;
+			
+			if( resultSet.next() ) {
+				member = new Member(	resultSet.getString("name"),
+													resultSet.getString("id"),
+													resultSet.getString("password"),
+													resultSet.getInt("firstSSN"),
+													resultSet.getInt("secondSSN"),
+													resultSet.getString("phone"),
+													resultSet.getString("email"),
+													resultSet.getString("address") );
+			}
+			
+			return member;			
+		} finally {
+			JdbcUtil.close(resultSet);
+			JdbcUtil.close(preparedStatement);			
+		} // end of finally
+		
+	} // end of selectById()
+	
+	// 회원 추가
+	public void insert(Connection connection, Member member) throws SQLException {
+		
+		query = "insert into member values(?,?,?,?,?,?,?,?)";
+		
+		// try-with-resource	
+		try( PreparedStatement preparedStatement = connection.prepareStatement(query) ) {
+			preparedStatement.setString( 1, member.getName() );
+			preparedStatement.setString( 2, member.getId() );
+			preparedStatement.setString( 3, member.getPassword() );
+			preparedStatement.setInt( 4, member.getFirstSSN() );
+			preparedStatement.setInt( 5, member.getSecondSSN() );
+			preparedStatement.setString( 6, member.getPhone() );
+			preparedStatement.setString( 7, member.getEmail() );
+			preparedStatement.setString( 8, member.getAddress() );
+		}
+		
+	} // end-of insert()
+}
